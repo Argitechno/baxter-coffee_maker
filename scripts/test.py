5c6252fe80ca6eb27be783e0deb283dfa3b003a9
Caleb Blondell <caleb.blondell@gmail.com>
2025-04-23 13:04:37 -0700

    Updated some info, trying to get a kcup in the planning scene.

--- scripts/test.py
+++ scripts/test.py
@@ -2,82 +2,53 @@
 
 import rospy
 import moveit_python
-import baxter_interface
-from geometry_msgs.msg import *
-
-def get_keurig_position():
-    """Current: Return a set position.
-    Future: Use Machine Vision to find the position of the Keurig
-
-    Returns:
-        geometry_msgs/Point: The position of the Keurig.
-    """
-    point = Point()
-    point.x = 0
-    point.y = 0
-    point.z = 0
-    return point
-    
-def get_cup_position():
-    """"Current: Return a set position.
-    Future: Use Machine Vision to find the position of the cup.
-
-    Returns:
-        geometry_msgs/Point: The position of the cup.
-    """
-    point = Point()
-    point.x = 0
-    point.y = 0
-    point.z = 0
-    return point
-
-def get_kcup_position():
-    """Current: Return a set position.
-    Future: Use Machine Vision to find the position of the K-Cup.
-
-    Returns:
-        geometry_msgs/Point: The position of the K-Cup.
-    """
-    point = Point()
-    point.x = 0
-    point.y = 0
-    point.z = 0
-    return point
+import baxter_interface 
+from geometry_msgs.msg import Pose
+from shape_msgs.msg import SolidPrimitive
 
 def add_keurig(p):
-    """Add the keurig to the planning scene given it's location.
+    """Add the keurig to the planning scene given it's location (center)
 
     Args:
         p PlanningSceneInterface: The planning scene to add the keurig to.+
     """
-    point = get_keurig_position()
+    #~~~ 9 X 14 Y 16 Z
+    pass 
+    #point = get_keurig_position()
 
 def add_cup(p):
-    """Add the Cup to the planning scene given it's location.
+    """Add the Cup to the planning scene given it's location (center)
 
     Args:
         p PlanningSceneInterface: The planning scene to add the Cup to.
     """
-    point = get_cup_position()
+    #~1.75 in rad * 4.5 cylinder
+    pass
+    #point = get_cup_position()
 
 
 def add_kcup(p):
-    """Add the K-Cup to the planning scene given it's location.
+    """Add the K-Cup to the planning scene at its proper pose.
 
     Args:
         p PlanningSceneInterface: The planning scene to add the K-Cup to.
     """
-    kcup_position = get_kcup_position()
+    #~1.0 in rad * 2 in cylinder
+   
+    # Cylinder, height = 2 in, rad = 1 in -> to meters
+    kcup = SolidPrimitive(kcup.CYLINDER,(0.0508, 0.0254))
+    kcup_pose = Pose((0.2, 1, -0.1), (0, 0, 0, 1))
+    p.addSolidPrimitive("K-Cup", kcup, kcup_pose, frame_id = "base")
+    p.sendUpdate(kcup, None, True)
 
 def main():
     """The idea here is to have baxter make some coffee."""
-
     # General planning scene for moving around the space, will include keurig bounding box so we move around it.
-    general_p = PlanningSceneInterface("base", "general_p");
+    general_p = PlanningSceneInterface("base", "general_p")
 
     #Add each object to our general planning scene.
-    add_keurig(general_p)
-    add_cup(general_p)
+    #add_keurig(general_p)
+    #add_cup(general_p)
     add_kcup(general_p)
 
     #Milestone 1. Great Job if we get this far!
@@ -96,7 +67,7 @@ def main():
 if __name__ == '__main__':
     try:
         print("Initializing Node...")
-        rospy.init_node("coffee_maker")
+        rospy.init_node("test_coffee_maker")
         main()
 
     except rospy.ROSInterruptException:
